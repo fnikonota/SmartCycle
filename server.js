42d0e0b3c781ef92a62cd0d0836e44706340be26
fnikonota <fnikonota@gmail.com>
2023-02-28 15:37:41 -0500

    serverFixes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // To start the server
 const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config(); 
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3000;
